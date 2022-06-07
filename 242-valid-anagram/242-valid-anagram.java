7cccf120392919ec7d5cc043daed12416704523d
Ahmed Sherif Abdelaty <a.sherif8@yahoo.com>
2022-06-07 14:44:28 +0200

    Time: 7 ms (50.79%), Space: 45 MB (33.43%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char [] chS = s.toCharArray();
+        char [] chT = t.toCharArray();
+        Arrays.sort(chS);
+        Arrays.sort(chT);
+        for (int i = 0 ; i < chS.length ; i++ ){
+            if (chS[i] != chT[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
