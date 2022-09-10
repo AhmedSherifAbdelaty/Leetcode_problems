3dbfafb21f81c9682dd7764babce375dc2002ef5
Ahmed Sherif Abdelaty <a.sherif8@yahoo.com>
2022-09-10 15:40:35 +0200

    Time: 3 ms (46.65%), Space: 42.8 MB (87.62%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,29 +1,26 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-           Set<Integer> set = copyFromSetToArr(nums);
+        Set<Integer> set = copyFromSetToArr(nums);
         List<Integer> res = new ArrayList<>();
         List<List<Integer>> allRes = new ArrayList<>();
-        permuteRec(nums,set,res ,allRes);
+        permuteRec(set,res ,allRes);
         return  allRes ;
     }
     
     
     
-    
-        private  void permuteRec(int[] nums, Set<Integer> set, List<Integer> res , List<List<Integer>> allRes) {
+    private void permuteRec(Set<Integer> set, List<Integer> res , List<List<Integer>> allRes) {
 
-        if (res.size() == set.size() ) {
-            Set<Integer> newSet = new HashSet<>(res);
-           if (newSet.size() == set.size()) {
-               List<Integer> newRes = new ArrayList<>(res);
-               allRes.add(newRes);
-           }
+        if ( set.isEmpty()  ) {
+            allRes.add(new ArrayList<>(res));
             return;
         }
 
-        for (int num : nums) {
+        for (int num : set) {
+            Set<Integer> copiedSet = new HashSet<>(set);
             res.add(num);
-            permuteRec(nums, set, res, allRes);
+            copiedSet.remove(num);
+            permuteRec( copiedSet, res, allRes);
             res.remove(res.size() - 1 );
         }
     }
