630cbd4d4e0854b2718fb227282ad75f1eadbf4e
Ahmed Sherif Abdelaty <a.sherif8@yahoo.com>
2022-09-17 17:36:07 +0200

    Time: 1 ms (100.00%), Space: 47.7 MB (65.20%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,32 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
       
-             int [] copyNum = Arrays.copyOf(nums , nums.length);
-        int duplicatedValue  = Integer.MIN_VALUE;
-        int duplicatedIndex  = 0 ;
-
-        if (nums.length == 1 ){
-            return 1;
+                   int track = 0, temp = -101;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != temp) {
+                nums[track++] = nums[i];
+            }
+            temp = nums[i];
         }
 
-        for (int i = 0 ; i < copyNum.length -1 ; i++){
-            if (copyNum[i] == copyNum[i+1]){
-                duplicatedValue = copyNum[i];
-            } else if (duplicatedValue != Integer.MIN_VALUE){
-                nums[duplicatedIndex] = duplicatedValue;
-                duplicatedValue = Integer.MIN_VALUE;
-                duplicatedIndex++;
-            } else {
-                nums[duplicatedIndex] = copyNum[i];
-                duplicatedIndex++;
-            }
-            if (  i + 1 == copyNum.length -1   ) {
-                nums[duplicatedIndex] = copyNum[i+1];
-                duplicatedIndex++;
-            }
-        }
-
-
-     return   duplicatedIndex ;   
+        return track;
     }
 }
