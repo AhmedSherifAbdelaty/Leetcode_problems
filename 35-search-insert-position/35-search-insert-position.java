003c50af0fee5312d80a2343a2bfe0f6640c130d
Ahmed Sherif Abdelaty <a.sherif8@yahoo.com>
2022-09-22 12:54:23 +0200

    Time: 0 ms (100.00%), Space: 42 MB (90.91%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,11 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-              int current  ;
+        int current  ;
       for (int i = 0 ; i < nums.length ; i++) {
           current = nums[i];
           if (nums[i] == target || current > target) return i ;
-          if (i == nums.length - 1 ) return nums.length ;
       }
-      return 0;
+      return nums.length;
     }
 }
